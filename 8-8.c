bbe305a483dce5563fae507c6b292dff2e8de2c4
priblue <33695989+priblue@users.noreply.github.com>
2017-11-16 01:07:22 +0900

    8-8
    배열이 왜 2차원이지

---
+++ 8-8.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	char names[10][20] = {"kim","lee","sin","jo","kim2","chae","jin","bak","so","chol"};
+	int i,j = 0;
+	char tmp[20];
+	
+	printf("\n배열 초기화\n");
+	for(j = 0; j < 10; j++){
+		printf("%5s",names[j]);
+	}
+	printf("\n");
+	
+	for(i = 0; i < 9; i++){
+		for(j = 0; j < 9 - 1; j++){
+			//문자배열 비교 
+		}
+	}
+	
+	printf("\nSorted Result\n");
+	for(j = 0; j < 10; j++){
+		printf("%5s",names[j]);
+	}
+	printf("\n");
+	
+	return 0;
+}
