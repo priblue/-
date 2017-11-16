2b805fcbc1e439cbaef162eafb81bdadd67d8db8
priblue <33695989+priblue@users.noreply.github.com>
2017-11-17 01:57:57 +0900

    Add files via upload

--- 8-8.c
+++ 8-8.c
@@ -8,7 +8,19 @@ int main(int argc, char *argv[]) {
 	char names[10][20] = {"kim","lee","sin","jo","kim2","chae","jin","bak","so","chol"};
 	int i,j = 0;
 	char tmp[20];
-	
+
+
+	for(int c = 0; c < 10; c++){
+		for(int d = 0; d < 20; d++){
+			if(names[c][d] == "\n"){
+				printf("z");
+			}else{
+				printf("%c",names[c][d]);
+			}
+			
+		}
+		printf("\n");
+	}
 	printf("\n배열 초기화\n");
 	for(j = 0; j < 10; j++){
 		printf("%5s",names[j]);
@@ -17,7 +29,7 @@ int main(int argc, char *argv[]) {
 	
 	for(i = 0; i < 9; i++){
 		for(j = 0; j < 9 - 1; j++){
-			//문자배열 비교 
+			//문자배열 비교
 		}
 	}
 	
