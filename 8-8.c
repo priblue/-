d441a7f48e119aa34cecfe685f0af3febf6dc9e5
priblue <33695989+priblue@users.noreply.github.com>
2017-11-17 19:45:42 +0900

    Update 8-8.c
    정렬 성공

--- 8-8.c
+++ 8-8.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
 int main(int argc, char *argv[]) {
 	char names[10][20] = {"kim","lee","sin","jo","kim2","chae","jin","bak","so","chol"};
 	int i,j = 0;
@@ -21,23 +19,35 @@ int main(int argc, char *argv[]) {
 		}
 		printf(" %d행\n",c);
 	}
+	
 	printf("\n배열 초기화\n");
 	for(j = 0; j < 10; j++){
 		printf("%5s",names[j]);
 	}
 	printf("\n");
-	
+
 	for(i = 0; i < 9; i++){
-		for(j = 0; j < 9 - 1; j++){
-			//문자배열 비교
+		for(j = 0; j < 9 - i; j++){
+			/*
+			names 배열에서 서로 인접한 두 문자열을 strcmp 함수로 처리해서 
+			양수가 나오는 경우는 names[j] 의 아스키 코드가 더 큰 경우이다.
+			다시 말해서, 
+			*/
+			if(strcmp(names[j],names[j + 1]) > 0){
+				//printf("위치 변경");
+				//tmp[j] = names[j];
+				//names[j] = names[j + 1];
+				//names[j + 1] = tmp[j];
+				strcpy(tmp,names[j]);
+				strcpy(names[j],names[j + 1]);
+				strcpy(names[j + 1],tmp);
+			}
 		}
 	}
-	
-	printf("\nSorted Result\n");
+	//printf("%s",names[2]); 
+	printf("\n정렬 완료\n");
 	for(j = 0; j < 10; j++){
 		printf("%5s",names[j]);
 	}
-	printf("\n");
 	
-	return 0;
 }
