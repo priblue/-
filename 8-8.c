178f3a6f380bace6e1cdca081051784cdcc1d275
priblue <33695989+priblue@users.noreply.github.com>
2017-11-18 11:08:07 +0900

    Update 8-8.c

--- 8-8.c
+++ 8-8.c
@@ -2,66 +2,70 @@
 #include <stdlib.h>
 #include <string.h>
 
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
 int main(int argc, char *argv[]) {
-	char names[10][20] = {"kim","lee","sin","jo","kim2","chae","jin","bak","so","chol"};
-	//char names[10][20] = {"apple","bag","console","drone","epic","false","console","holic","inner","join"};//2번 같은 문자 포함. 
-	//char names[10][20] = {"Winter","is","comming","Too","cold","to","run","computer","pubg","game"};//1번 두 글자 대문자인 경우. 
-	 
+	char names[10][20] = {"Kim","lee","sin","jo","kim2","Chae","jin","bak","so","chol"};
+	char tmp[20];//names Bubble sorting 
+	char tmp2[20];
 	int i,j = 0;
-	char tmp[20];
+	
+	char names_tmp[10][20];//names 배열 복사.
 
-
-	printf("==========names 배열의 구조==========\n\n");
-	for(int c = 0; c < 10; c++){//Display "names" array with null to '#'
+	for(int c = 0; c < 10; c++){
 		for(int d = 0; d < 20; d++){
-			if(names[c][d] == 0){
-				printf("#");
+			if(names[c][d] == '0'){
+				printf("z");
 			}else{
 				printf("%c",names[c][d]);
 			}
 			
 		}
-		printf(" %d행\n",c);
+		printf("\n");
 	}
-
 	
 	printf("\n배열 초기화\n");
-	for(int j = 0; j < 10; j++){
-		printf("%5s ",names[j]);
+	for(j = 0; j < 10; j++){
+		printf("%5s",names[j]);
 	}
 	printf("\n");
 	
-	char names_tmp[10][20];//names 배열 복사.
-			for(int i = 0; i < 10; i++){
-				for(int j = 0; j < 20; j++){
-					names_tmp[i][j] = names[i][j];
-				}
-				strlwr(names_tmp[i]);//복사된 배열을 모두 대문자로 변경.
-			}
-			printf("대문자로 변경\n");
-			for(int i = 0; i < 10; i++){
-				printf("%5s ",names_tmp[i]);
-			}
-			
-
-	for(int i = 0; i < 9; i++){
-		for(int j = 0; j < 9 - i; j++){
-			/*
-			names 배열에서 서로 인접한 두 문자열을 strcmp 함수로 처리해서
-			양수가 나오는 경우는 names[j] 의 아스키 코드가 더 큰 경우이다.
-			다시 말해서, 65 132
-			*/
-			if(strcmp(names_tmp[j], names_tmp[j + 1]) > 0){
+ 	for(int i = 0; i < 10; i++){
+ 		for(int j = 0; j < 20; j++){
+ 			names_tmp[i][j] = names[i][j];
+ 		}
+ 		strupr(names_tmp[i]);//복사된 배열을 모두 대문자로 변경.
+ 	}
+ 	printf("대문자로 변경\n");
+ 	for(int i = 0; i < 10; i++){
+ 		printf("%5s ",names_tmp[i]);
+ 	}
+	
+	
+	for(i = 0; i < 9; i++){
+		for(j = 0; j < 9 - i; j++){
+			if(strcmp(names_tmp[j],names_tmp[j + 1]) > 0){
+				strcpy(tmp2,names_tmp[j]);
+  				strcpy(names_tmp[j],names_tmp[j + 1]);
+  				strcpy(names_tmp[j + 1],tmp2);
+  				
 				strcpy(tmp,names[j]);
-				strcpy(names[j],names[j + 1]);
-				strcpy(names[j + 1],tmp);
+  				strcpy(names[j],names[j + 1]);
+  				strcpy(names[j + 1],tmp);
 			}
-			
 		}
 	}
-	printf("\n정렬 완료\n");
-	for(int j = 0; j < 10; j++){
-		printf("%5s ",names[j]);
-	}
 	
+	printf("\nSorted Result\n");
+	for(j = 0; j < 10; j++){
+		printf("%5s",names[j]);
+	}
+	printf("\nsort tmp\n");
+	
+	for(j = 0; j < 10; j++){
+		printf("%5s",names_tmp[j]);
+	}
+	printf("\n");
+	
+	return 0;
 }
