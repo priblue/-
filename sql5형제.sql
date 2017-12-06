4a475f3104b91de04817273d339f6706fac7d8cd
priblue <33695989+priblue@users.noreply.github.com>
2017-12-06 22:08:59 +0900

    Create sql5형제.sql

---
+++ sql5형제.sql
@@ -0,0 +1,31 @@
+--2. 매출이 가장 높은 날이 언제인지 알 수 있게 날짜와 총 매출을 표시하시오.
+select cust.order_date, sum(food.food_price) 총_매출
+from cust_order cust, food_list food
+group by cust.order_date
+order by 총_매출 desc
+
+--3.예약한 손님중 포장을 하는 사람 중 가장 비싼 음식을 시킨 주문 번호와, 음식 이름, 가격을 표시하세요.
+select top 1 cust.order_num, food.food_name,food.food_price
+from cust_order cust INNER JOIN food_list food ON cust.food_id = food.food_id
+where cust.SMid = (select serve.SMid from serve_method serve where serve.method = '포장')
+AND
+cust.OSid = (Select order_state.OSid from order_state where order_state.order_state = '예약')
+order by food.food_price desc
+
+--현재 매장에 들어온 주문들을 전체적으로 조회하라.
+
+select food_list.food_name,  cust_order.order_num,  cust_order.order_date,  payment_method.method,  order_state.order_state,  serve_method.method,cust_order.order_address
+from cust_order INNER JOIN food_list ON cust_order.food_id = food_list.food_id
+				INNER JOIN payment_method ON cust_order.PMid = payment_method.PMid
+				INNER JOIN serve_method ON cust_order.SMid = serve_method.SMid
+				INNER JOIN order_state ON cust_order.OSid = order_state.OSid
+
+--1. 각 메뉴 중 가장 잘나가는 음식(매출이 높은)순으로 음식 이름과 가격을 표시하시오.
+
+select cust.food_id ,food.food_name 메뉴,count(*) * food.food_price 총매출
+from cust_order cust inner join food_list food on cust.food_id = food.food_id
+group by food.food_price, food.food_name, cust.food_id
+order by 총매출 desc
+
+--
+
